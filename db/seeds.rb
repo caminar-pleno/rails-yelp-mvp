0911d589a4aa5cf296e65eb6bf1a4fbe77e1376f
Gina Vargas <caminar.pleno@gmail.com>
2021-07-30 19:27:28 -0400

    restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+50.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end

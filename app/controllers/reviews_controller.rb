b4302030bfb3f3f8a7b6c1b3748181207e5cdee1
Gina Vargas <caminar.pleno@gmail.com>
2021-07-30 23:25:36 -0400

    add review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,37 @@
 class ReviewsController < ApplicationController
+before_action :set_review, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def destroy
+      @review = Review.find(params[id])
+      @review.destroy
+      redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[restaurant_id])
+  end
 end

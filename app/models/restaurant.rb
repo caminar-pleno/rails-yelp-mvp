4c1cb0fadbd19eb3082f23503e799c8d39bbbd0a
Gina Vargas <caminar.pleno@gmail.com>
2021-08-02 13:00:20 -0400

    validates

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence:  { presence: true, message: 'must be not space' }
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
+    message: "%{value} is not a valid category" }
 end
